d80f29f9c9e3d58a7257f91994ee3a3e9af939f5
Harold Bemis <emailofnick@gmail.com>
2016-07-22 16:11:53 -0500

    Changed the ground to a box to help prevent enemies from falling through on spawn (seems very effective) Changed the scene to accommodate this - also changed the player to spawn in the middle of the map

--- Assets/Scripts/Ground/GroundInit.cs
+++ Assets/Scripts/Ground/GroundInit.cs
@@ -10,6 +10,6 @@ public class GroundInit : MonoBehaviour {
     {
         globals = Globals.Instance;
         trans = GetComponent<Transform>();
-        trans.localScale = new Vector3(globals.GROUNDXSIZE/10,1, globals.GROUNDZSIZE/10);
+        trans.localScale = new Vector3(globals.GROUNDXSIZE,1, globals.GROUNDZSIZE);
     }
 }
