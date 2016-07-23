3220b1d5c80eda4cb7669513e27131fe02cadae3
matt <spree33@live.com>
2016-07-23 16:37:42 -0500

    shiii

---
+++ Assets/Scripts/SpecialMoves/Blast.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blast : MonoBehaviour {
+
+    public float force = 100.0f;
+    public float radius = 5.0f;
+    public ForceMode forceMode;
+        
+	
+	public void blast_test ()
+    {
+        Vector3 blast_location = transform.position;
+        blast_location.y += 0.7f;
+
+        foreach (Collider col in Physics.OverlapSphere(transform.position, radius))
+        {
+            if (col.GetComponent<Rigidbody>() != null)
+            {
+                col.GetComponent<Rigidbody>().AddExplosionForce(force, blast_location, radius, 0.0f, forceMode);
+            }
+        }  
+	}
+	
+
+}
